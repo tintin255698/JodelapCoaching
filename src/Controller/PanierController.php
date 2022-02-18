e2e84a0d7bbd65dfed9599208a0c5b6397234174
tintin255698 <vivien.joly@hotmail.fr>
2022-02-18 13:56:35 +0100

    v1xc

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -35,8 +35,81 @@ class PanierController extends AbstractController
             $this->addFlash('info', "Vous devez être connecté pour accéder au récapitulatif de votre réservation");
             return  $this->redirectToRoute('app_login');
         } else {
-            require ('panier.php');
-        }
+           //Récupère les paniers
+           $coffret = $session->get('coffret', []);
+           $evenement = $session->get('evenement', []);
+           $coaching = $session->get('coaching', []);
+
+//coffret
+
+           $coffretWithData = [];
+
+//Transforme le panier en array
+           foreach ($coffret as $id => $quantity) {
+               $coffretWithData[] = [
+                   'product' => $coffretRepository->find($id),
+                   'quantity' => $quantity
+               ];
+           }
+
+//Calcul total
+           $totalCoffret = 0;
+           foreach ($coffretWithData as $item) {
+               $totalItem3 = $item['product']->getPrix() * $item['quantity']['heure'];
+               $totalCoffret += $totalItem3;
+           }
+
+//Evenement
+           $evenementWithData = [];
+
+//Transforme Evenement en array
+           foreach ($evenement as $id => $quantity) {
+               $evenementWithData[] = [
+                   'product' => $evenementRepository->find($id),
+                   'quantity' => $quantity
+               ];
+           }
+
+//Calcul total evenement
+           $totalEvenement = 0;
+           foreach ($evenementWithData as $item) {
+               $totalItem2 = $item['product']->getPrix() * $item['quantity'];
+               $totalEvenement += $totalItem2;
+           }
+
+//Coaching
+           $coachingWithData = [];
+
+
+//Transforme le panier en array
+           foreach ($coaching as $id => $quantity) {
+               $find = $coachingTarifRepository->find($id);
+               if($quantity['personne'] >2){
+                   $prix = $find->getPriceForTwo()+($find->getPriceForThree()*($quantity['personne']-2));
+               }else{
+                   $prix =  $find->getPriceForTwo();
+               }
+               $coachingWithData[] = [
+                   'product' => $find,
+                   'quantity' => $quantity['quantity'],
+                   'personne' => $quantity['personne'],
+                   'prix' => $prix
+               ];
+           }
+
+//Calcul total
+
+           $sum = array_sum(array_column($coachingWithData, 'prix'));
+
+//Total general
+
+
+           $tot = ($sum + $totalEvenement/100 + $totalCoffret/100)*100;
+
+
+           $tva = $tot*0.2;
+
+       }
 
         return $this->render('panier/index.html.twig', [
             'evenement' => $evenementWithData,
@@ -69,7 +142,80 @@ class PanierController extends AbstractController
                 $entityManager->persist($user);
                 $entityManager->flush();
 
-                require('panier.php');
+                //Récupère les paniers
+                $coffret = $session->get('coffret', []);
+                $evenement = $session->get('evenement', []);
+                $coaching = $session->get('coaching', []);
+
+//coffret
+
+                $coffretWithData = [];
+
+//Transforme le panier en array
+                foreach ($coffret as $id => $quantity) {
+                    $coffretWithData[] = [
+                        'product' => $coffretRepository->find($id),
+                        'quantity' => $quantity
+                    ];
+                }
+
+//Calcul total
+                $totalCoffret = 0;
+                foreach ($coffretWithData as $item) {
+                    $totalItem3 = $item['product']->getPrix() * $item['quantity']['heure'];
+                    $totalCoffret += $totalItem3;
+                }
+
+//Evenement
+                $evenementWithData = [];
+
+//Transforme Evenement en array
+                foreach ($evenement as $id => $quantity) {
+                    $evenementWithData[] = [
+                        'product' => $evenementRepository->find($id),
+                        'quantity' => $quantity
+                    ];
+                }
+
+//Calcul total evenement
+                $totalEvenement = 0;
+                foreach ($evenementWithData as $item) {
+                    $totalItem2 = $item['product']->getPrix() * $item['quantity'];
+                    $totalEvenement += $totalItem2;
+                }
+
+//Coaching
+                $coachingWithData = [];
+
+
+//Transforme le panier en array
+                foreach ($coaching as $id => $quantity) {
+                    $find = $coachingTarifRepository->find($id);
+                    if($quantity['personne'] >2){
+                        $prix = $find->getPriceForTwo()+($find->getPriceForThree()*($quantity['personne']-2));
+                    }else{
+                        $prix =  $find->getPriceForTwo();
+                    }
+                    $coachingWithData[] = [
+                        'product' => $find,
+                        'quantity' => $quantity['quantity'],
+                        'personne' => $quantity['personne'],
+                        'prix' => $prix
+                    ];
+                }
+
+//Calcul total
+
+                $sum = array_sum(array_column($coachingWithData, 'prix'));
+
+//Total general
+
+
+                $tot = ($sum + $totalEvenement/100 + $totalCoffret/100)*100;
+
+
+                $tva = $tot*0.2;
+
 
                 //Ref commande
                 $debut = new DateTime('now');
@@ -177,7 +323,80 @@ class PanierController extends AbstractController
 
       $commande = $coach[0]->getNumeroDeCommande();
 
-    require('panier.php');
+            //Récupère les paniers
+            $coffret = $session->get('coffret', []);
+            $evenement = $session->get('evenement', []);
+            $coaching = $session->get('coaching', []);
+
+//coffret
+
+            $coffretWithData = [];
+
+//Transforme le panier en array
+            foreach ($coffret as $id => $quantity) {
+                $coffretWithData[] = [
+                    'product' => $coffretRepository->find($id),
+                    'quantity' => $quantity
+                ];
+            }
+
+//Calcul total
+            $totalCoffret = 0;
+            foreach ($coffretWithData as $item) {
+                $totalItem3 = $item['product']->getPrix() * $item['quantity']['heure'];
+                $totalCoffret += $totalItem3;
+            }
+
+//Evenement
+            $evenementWithData = [];
+
+//Transforme Evenement en array
+            foreach ($evenement as $id => $quantity) {
+                $evenementWithData[] = [
+                    'product' => $evenementRepository->find($id),
+                    'quantity' => $quantity
+                ];
+            }
+
+//Calcul total evenement
+            $totalEvenement = 0;
+            foreach ($evenementWithData as $item) {
+                $totalItem2 = $item['product']->getPrix() * $item['quantity'];
+                $totalEvenement += $totalItem2;
+            }
+
+//Coaching
+            $coachingWithData = [];
+
+
+//Transforme le panier en array
+            foreach ($coaching as $id => $quantity) {
+                $find = $coachingTarifRepository->find($id);
+                if($quantity['personne'] >2){
+                    $prix = $find->getPriceForTwo()+($find->getPriceForThree()*($quantity['personne']-2));
+                }else{
+                    $prix =  $find->getPriceForTwo();
+                }
+                $coachingWithData[] = [
+                    'product' => $find,
+                    'quantity' => $quantity['quantity'],
+                    'personne' => $quantity['personne'],
+                    'prix' => $prix
+                ];
+            }
+
+//Calcul total
+
+            $sum = array_sum(array_column($coachingWithData, 'prix'));
+
+//Total general
+
+
+            $tot = ($sum + $totalEvenement/100 + $totalCoffret/100)*100;
+
+
+            $tva = $tot*0.2;
+
 
 
             $email = (new TemplatedEmail())
